d0c94d63c3b1c48b51a238b8d695d94403087fac
Rafik Rizik <rizik944@gmail.com>
2019-09-07 21:02:38 -0500

    re did some events stuff and added text input event thats being used in console

--- flux/console.py
+++ flux/console.py
@@ -42,12 +42,12 @@ class Console:
         self.console_background = pygame.draw.rect(Display.fake_display, (49, 103, 250), (0, 0, Display.x, self.y))
         self.console_textfield = pygame.draw.rect(Display.fake_display, (30, 201, 181), (0, self.y - 20, Display.x, 20))
 
+        key = events.handle_text_input_event("layer_999")
+        if key:
+            self.text += key
         if events.key_pressed_once("BACKSPACE", "layer_999"):
             self.text = self.text[:-1]
-        elif events.key_pressed_any("layer_999"):
-            self.text += str(events.get_keystroke())
-
-        if events.key_pressed_once("RETURN", "layer_999"):
+        if events.key_pressed_once("RETURN", "layer_999") and len(self.text):
             self.history.append(self.text)
             self.text = ""
 
--- flux/events.py
+++ flux/events.py
@@ -10,12 +10,11 @@ class Events:
         self.keys_pressed = []
         self.mouse_pressed = []
         self.events_triggered = []
+
         self.MONE = 1
         self.MMIDDLE = 2
         self.MTWO = 3
-        self.events_list = []
-        self.keystroke = None
-        self.keystroke_list = []
+        self.TEXT_INPUT_EVENT = pygame.USEREVENT + 1
 
         self.assign_keys()
 
@@ -24,21 +23,15 @@ class Events:
             if "K_" in name:
                 new_name = name.replace("K_", "")
                 self.__dict__[new_name] = pygame.__dict__[name]
-            else:
-                constant = pygame.__dict__[name]
-                if type(constant) == int:
-                    self.events_list.append(constant)
-                    self.__dict__[name] = pygame.__dict__[name]
 
-    def key_pressed_any(self, layer="layer_0"):
+    def handle_text_input_event(self, layer="layer_0"):
         if layer == Layer.get_layer() or layer == "layer_all":
-            return True if len(self.keystroke_list) > 0 else False
-        return False
+            value = self.__dict__.get("TEXT_INPUT_EVENT")
+            for event in self.events_triggered:
+                if event.type == value:
+                    return event.key
 
-    def get_keystroke(self):
-        key = self.keystroke_list[0]
-        self.keystroke_list = self.keystroke_list[1:]
-        return str(key)
+        return None
 
     # capital letters are not working
     def key_pressed(self, key, layer="layer_0"):
@@ -58,9 +51,13 @@ class Events:
         value = self.__dict__.get(button)
         return value in self.mouse_pressed
 
-    def event_triggered(self, event):
-        value = self.__dict__.get(event)
-        return value in self.events_triggered
+    def event_triggered(self, event_name):
+        value = self.__dict__.get(event_name)
+        for event in self.events_triggered:
+            if value == event.type:
+                return True
+
+        return False
 
     def register_buttons_pressed(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -71,22 +68,22 @@ class Events:
     def register_keys_pressed(self, event):
         if event.type == pygame.KEYDOWN:
             self.keys_pressed.append(event.key)
-            if str(event.unicode) in (string.digits + string.ascii_letters) or str(event.unicode) == " ":
-                self.keystroke_list.append(str(event.unicode))
         if event.type == pygame.KEYUP and event.key in self.keys_pressed:
             self.keys_pressed.remove(event.key)
 
-    def register_events_triggered(self, event):
-        if event.type in self.events_list:
-            self.events_triggered.append(event.type)
-        else:
-            self.events_triggered.remove(event.type)
+    def register_text_input_event(self, event):
+        if event.type == pygame.KEYDOWN:
+            if str(event.unicode) in (string.digits + string.ascii_letters + " "):
+                text_input_event = pygame.event.Event(self.TEXT_INPUT_EVENT, {"key": str(event.unicode)})
+                pygame.event.post(text_input_event)
 
     def update(self):
-        for event in pygame.event.get():
-            self.register_events_triggered(event)
+        self.events_triggered = pygame.event.get()
+
+        for event in self.events_triggered:
             self.register_buttons_pressed(event)
             self.register_keys_pressed(event)
+            self.register_text_input_event(event)
 
 
 events = Events()
