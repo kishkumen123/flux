f2c96be02b883f913c49ffaa72b2eb8db92dece7
Rafik Rizik <rizik944@gmail.com>
2019-09-11 17:33:52 -0500

    error checking for command argument lengths

--- flux/commands.py
+++ flux/commands.py
@@ -1,8 +1,9 @@
 import globals
-
+#from console import console
 
 commands = []
 
+#all these prints need to be changed to console.add_to_history() once we fix the circular import issue
 
 class CommandInfo:
     name = None
@@ -12,13 +13,13 @@ class CommandInfo:
 
 
 def init_commands():
-    add_command("ls", command_ls, 1, 16)
+    add_command("ls", command_ls, 1, 1)
     add_command("quit", command_quit)
 
 
 def run_command(command_string):
 
-    command_array = command_string.split(" ")
+    command_array = list(filter(None, command_string.split(" ")))
     if not len(command_array): return
 
     command_name = command_array[0]
@@ -27,10 +28,17 @@ def run_command(command_string):
 
     for command in commands:
         if command_name == command.name:
-            argument_string = "" if argument_count == 1 else "s"
-            if argument_count < command.arg_count_min:
-                print("Error: %s requires at least %s argument%s - %s given" % (command_name, command.arg_count_min, argument_string, argument_count))
+
+            if argument_count < command.arg_count_min or argument_count > command.arg_count_max:
+                if command.arg_count_min == command.arg_count_max:
+                    min_string = "" if command.arg_count_min == 1 else "s"
+                    print("Error: %s requires exactly %s argument%s - %s given" % (command_name, command.arg_count_min, min_string, argument_count))
+                    return
+                min_string = "" if command.arg_count_min == 1 else "s"
+                max_string = "" if command.arg_count_max == 1 else "s"
+                print("Error: %s requires at least %s argument%s and at most %s argument%s - %s given" % (command_name, command.arg_count_min, min_string, command.arg_count_max, max_string, argument_count))
                 return
+
             command.proc(non_command_arguments)
             return
     print("Command %s - not known" % command_name)
@@ -45,9 +53,8 @@ def add_command(name, proc, arg_count_min=0, arg_count_max=0):
     commands.append(info)
 
 
-def command_output(command):
-    from console import console
-    console.add_to_histroy(command)
+#def command_output(command):
+    #console.add_to_histroy(command)
 
 
 def command_ls(arguments):
@@ -56,12 +63,11 @@ def command_ls(arguments):
 
 
 def command_quit(arguments):
-    command_output("Quiting Flux")
+    #command_output("Quiting Flux")
     print("We called quit. existing flux!")
     globals.running = False
 
 
-
-#init_commands()
-#run_command("na")
-#run_command("ls")
+init_commands()
+run_command("na")
+run_command("ls")
